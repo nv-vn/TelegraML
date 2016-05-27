7fbbb32ce9348cc32fdd36bcd1216347f381b384
nv-vn <nv@cock.li>
2016-05-27 12:50:31 -0400

    Add with_auth test; bump version

--- example/bot.ml
+++ example/bot.ml
@@ -13,17 +13,20 @@ module MyBot = Mk (struct
 
   let commands =
     let say_hi = function
-      | {chat} -> SendMessage (chat.id, "Hi", false, None, None) in
-    let my_pics = function
+      | {chat} -> SendMessage (chat.id, "Hi", false, None, None)
+    and my_pics = function
       | {chat; from = Some {id}} ->
         GetUserProfilePhotos (id, None, None,
                               function
                               | Result.Success photos ->
                                 SendMessage (chat.id, "Your photos: " ^ string_of_int photos.total_count, false, None, None)
                               | _ -> SendMessage (chat.id, "Couldn't get your profile pictures!", false, None, None))
-      | {chat} -> SendMessage (chat.id, "Couldn't get your profile pictures!", false, None, None) in
+      | {chat} -> SendMessage (chat.id, "Couldn't get your profile pictures!", false, None, None)
+    and check_admin = function
+      | {chat} -> SendMessage (chat.id, "Congrats, you're an admin!", false, None, None) in
     [{name = "say_hi"; description = "Say hi!"; enabled = true; run = say_hi};
-     {name = "my_pics"; description = "Count profile pictures"; enabled = true; run = my_pics}]
+     {name = "my_pics"; description = "Count profile pictures"; enabled = true; run = my_pics};
+     {name = "admin"; description = "Check whether you're an admin"; enabled = true; run = with_auth ~command:check_admin}]
 end)
 
 let () = MyBot.run ()
